890b1cb7db1034e59d3987264e2445acf3b64551
renoXD1 <renoafgaria@gmail.com>
2025-11-29 19:43:36 +0700

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,5 @@
+import "./globals.css";
+export const metadata = { title: "DeepSeek Web Chat Bot" };
+export default function RootLayout({ children }) {
+  return <html lang="id"><body>{children}</body></html>;
+}
