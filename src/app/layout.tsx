322307d983122d1defc66eb475d0ac979e002e73
renoXD1 <renoafgaria@gmail.com>
2025-11-29 19:37:19 +0700

    Delete src/app directory

--- src/app/layout.tsx
+++
@@ -1,5 +0,0 @@
-import "./globals.css";
-export const metadata = { title: "DeepSeek Web Chat Bot" };
-export default function RootLayout({ children }) {
-  return <html lang="id"><body>{children}</body></html>;
-}
